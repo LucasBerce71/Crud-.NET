1bdd28ff0c6dd2290e9346c3f75a306b8ec91782
Lucas Bercê de Jesus <lucasberce71@gmail.com>
2023-06-28 09:43:33 -0300

    first commit

---
+++ Modelo.Domain/Interfaces/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Modelo.Domain.Entities;
+
+namespace Modelo.Domain.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        void Insert(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(int id);
+        TEntity Select(int id);
+    }
+}
+
+namespace Modelo.Domain.Interfaces
+{
+    public interface IBaseService<TEntity> where TEntity : BaseEntity
+    {
+        TEntity Add<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+        void Delete(int id);
+        IList<TEntity> Get();
+        TEntity GetById(int id);
+        TEntity Update<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+    }
+}
---
+++ Modelo.Infra.Data/Context/MySqlContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Modelo.Infra.Data.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options): base(options) {}
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+        }
+    }
+}
